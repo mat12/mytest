b405f08bb8b52ddb5751a5a8e6b865d1cafd04b8
formiano <formianodicastellone@hotmail.it>
2015-08-18 15:15:46 +0200

    mytest

---
+++ lib/python/Tools/StbHardware.py
@@ -0,0 +1,97 @@
+from fcntl import ioctl
+from struct import pack, unpack
+from Components.config import config
+from boxbranding import getBoxType, getBrandOEM
+
+def getFPVersion():
+	ret = None
+	try:
+		ret = long(open("/proc/stb/fp/version", "r").read())
+	except IOError:
+		try:
+			fp = open("/dev/dbox/fp0")
+			ret = ioctl(fp.fileno(),0)
+		except IOError:
+			print "getFPVersion failed!"
+	return ret
+
+def setFPWakeuptime(wutime):
+	try:
+		open("/proc/stb/fp/wakeup_time", "w").write(str(wutime))
+	except IOError:
+		try:
+			fp = open("/dev/dbox/fp0")
+			ioctl(fp.fileno(), 6, pack('L', wutime)) # set wake up
+		except IOError:
+			print "setFPWakeupTime failed!"
+
+def setRTCoffset():
+	import time
+	if time.localtime().tm_isdst == 0:
+		forsleep = 7200+time.timezone
+	else:
+		forsleep = 3600-time.timezone
+
+	t_local = time.localtime(int(time.time()))
+
+	print "set RTC to %s (rtc_offset = %s sec.)" % (time.strftime("%Y/%m/%d %H:%M", t_local), forsleep)
+
+	# Set RTC OFFSET (diff. between UTC and Local Time)
+	try:
+		open("/proc/stb/fp/rtc_offset", "w").write(str(forsleep))
+	except IOError:
+		print "set RTC Offset failed!"
+
+def setRTCtime(wutime):
+	#if getBoxType() in ('gb800solo', 'gb800se', 'gb800ue'):
+	#	setRTCoffset() 
+	try:
+		open("/proc/stb/fp/rtc", "w").write(str(wutime))
+	except IOError:
+		try:
+			fp = open("/dev/dbox/fp0")
+			ioctl(fp.fileno(), 0x101, pack('L', wutime)) # set wake up
+		except IOError:
+			print "setRTCtime failed!"
+
+def getFPWakeuptime():
+	ret = 0
+	try:
+		ret = long(open("/proc/stb/fp/wakeup_time", "r").read())
+	except IOError:
+		try:
+			fp = open("/dev/dbox/fp0")
+			ret = unpack('L', ioctl(fp.fileno(), 5, '    '))[0] # get wakeuptime
+		except IOError:
+			print "getFPWakeupTime failed!"
+	return ret
+
+wasTimerWakeup = None
+
+def getFPWasTimerWakeup():
+	global wasTimerWakeup
+	if wasTimerWakeup is not None:
+		return wasTimerWakeup
+	wasTimerWakeup = False
+	try:
+		wasTimerWakeup = int(open("/proc/stb/fp/was_timer_wakeup", "r").read()) and True or False
+	except:
+		try:
+			fp = open("/dev/dbox/fp0")
+			wasTimerWakeup = unpack('B', ioctl(fp.fileno(), 9, ' '))[0] and True or False
+		except IOError:
+			print "wasTimerWakeup failed!"
+	if wasTimerWakeup:
+		# clear hardware status
+		clearFPWasTimerWakeup()
+	return wasTimerWakeup
+
+def clearFPWasTimerWakeup():
+	try:
+		open("/proc/stb/fp/was_timer_wakeup", "w").write('0')
+	except:
+		try:
+			fp = open("/dev/dbox/fp0")
+			ioctl(fp.fileno(), 10)
+		except IOError:
+			print "clearFPWasTimerWakeup failed!"
